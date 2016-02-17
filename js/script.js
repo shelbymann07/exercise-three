ea265e6bd0caa7d5335a2a52f36fc23f51526de8
Shelby Mann <smann@Shelbys-MacBook-Pro.local>
2016-02-16 19:46:52 -0600

    first commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,35 @@
 $(document).ready(function() {
-    console.log("Hello world.")
+    getData();
 });
 
 
+function getData() {
 
+    $.getJSON("js/columbia.json", function(data) {
+        //Callback is a function that runs after something else executes. So after the data is loaded.
+        loopThroughData(data);
+    })
 
+}
 
+function loopThroughData(s) {
+    
+    for (i=0; i<s.length; i++) {
+        var schoolName = s[i]["SCHOOL_NAME"];
+        var year = s[i]["YEAR"];
+        var gradPct = s[i]["GRADUATES_PCT"];
+        
+        //If graduation percent is less than (<) 50%... && indicates 'and'
+        //if (gradPct < 60 && gradPct > 0) {
+            //...do this.
+        //    console.log(year, schoolName, gradPct);
+        //}
+        
+        var barWidth = gradPct * 10;
+        
+        if (schoolName === "FREDERICK DOUGLASS HIGH") {
+            $(".chart").append("<div class='bar' style='width:"+barWidth+"px'></div>");
+        }
+    }
+}
 
